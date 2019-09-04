e3a91a5c872e804cdc1e002b2a7e32597b825500
Boarders <callan.mcgill@gmail.com>
2019-09-04 17:26:58 -0400

    Add ability to convert to SKI combinators

--- src/Untyped/Combinators.hs
+++ src/Untyped/Combinators.hs
@@ -1,8 +1,71 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Untyped.Combinators
-import Parser.Lambda.Untyped
+
+module Untyped.Combinators where
+
+import Untyped.Expression
+import Data.Set
 
 data SKI = S | K | I
+  deriving Show
 
-toSKI
+data SKIExpression a =
+    SKIVar a
+  | SKILam a (SKIExpression a)
+  | SKIApp   (SKIExpression a) (SKIExpression a)
+  | SKI  SKI
+
+freeVars :: (Ord a) => SKIExpression a -> Set a
+freeVars (SKIVar a)      = singleton a
+freeVars (SKILam b body) = b `delete` (freeVars body)
+freeVars (SKIApp l r)  = (freeVars l) `union` (freeVars r)
+freeVars (SKI _)     = mempty
+
+toSKIRep :: (Ord a) => ExpressionF a -> SKIExpression a
+toSKIRep = elimBoundVars . toSKI
+
+-- TODO : let expressions
+printSKI :: (Show a) => SKIExpression a -> String
+printSKI (SKIVar v)      = show v
+printSKI (SKILam b body) = unwords ["λ", show b , "→ ", printSKI body]
+printSKI (SKIApp l r)    = unwords ["(" <> printSKI l, printSKI r <> ")"]
+printSKI (SKI ski) = show ski
+
+
+
+elimBoundVars :: (Ord a) => SKIExpression a -> SKIExpression a
+elimBoundVars = \case
+  SKILam v (SKIVar v') | v == v' -> SKI I
+  SKILam v e | v `notMember` freeVars e -> SKIApp (SKI K) (elimBoundVars e)
+  SKILam v (SKIApp l r)
+    -> elimBoundVars (SKIApp (SKIApp (SKI S) (SKILam v l)) (SKILam v r))
+  SKILam v body -> elimBoundVars (SKILam v (elimBoundVars body))
+  (SKIApp l r) -> SKIApp (elimBoundVars l) (elimBoundVars r)
+  ski          -> ski
+
+
+toSKI :: ExpressionF a -> SKIExpression a
+toSKI (Var v)   = (SKIVar v)
+toSKI (App l r) = SKIApp (toSKI l) (toSKI r)
+toSKI (Lam b f) = SKILam b (toSKI f)
+toSKI (Let _ _) = error "to do: let expressions"
+  
+
+abstract :: (Eq a) => a -> SKIExpression a -> SKIExpression a
+abstract var (SKIVar name)
+  | var == name = SKI I
+  | otherwise   = SKIApp (SKI K) (SKIVar name)
+abstract var (SKIApp l r) = SKIApp (abstract var l) (abstract var r)
+abstract var (SKILam v body)
+  | var == v  = abstract var body
+  | otherwise = SKILam var (abstract var body)
+abstract _ ski@(SKI _) = ski
+
+
+eliminateBoundVars :: (Eq a) => SKIExpression a -> SKIExpression a
+eliminateBoundVars = \case
+  SKILam a body -> eliminateBoundVars (abstract a body)
+  SKIApp l r    -> SKIApp (eliminateBoundVars l) (eliminateBoundVars r)
+  expression    -> expression
+  
+
