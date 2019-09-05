d3b051e938c422ae73bb73b4274f1ecce360a04a
Boarders <callan.mcgill@gmail.com>
2019-09-04 20:14:09 -0400

    Add galaxy brain approach

--- src/Untyped/Expression.hs
+++ src/Untyped/Expression.hs
@@ -1,5 +1,14 @@
+{-# LANGUAGE TypeFamilies    #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE DerivingStrategies #-}
+
 module Untyped.Expression where
 
+import Data.Kind
 import Data.Text
 
 data ExpressionF a =
@@ -22,3 +31,59 @@ type Identifier = Text
 type Expression = ExpressionF Identifier
 type Program    = ProgramF Identifier
 type Block      = BlockF ExpressionF Identifier
+
+
+data Var b a =
+    B b
+  | F a
+  deriving (Eq, Ord, Show)
+
+newtype Scope
+          (b :: Type)
+          (f :: k -> Type -> Type)
+          (i :: k)
+          (a :: Type)
+  = Scope {getScope :: f i (Var b (f i a))}
+
+data ExpX i a
+  = LitX (XLit i a)
+  | BVarX (XBVar i a)
+  | FVarX (XFVar i a)
+  | LamX (XAbs i a) (ScopeX (XBVar i a) ExpX i a)
+  | AppX (XApp i a) (ExpX i a) (ExpX i a)
+  | ExpX (XExp i a)
+
+
+type family XLit i a
+type family XBVar i a
+type family XFVar i a
+type family XAbs i a
+type family XApp i a
+type family XExp i a
+type family ScopeX (b :: Type) (f :: k -> Type -> Type) (i :: k) (a :: Type) :: Type
+
+data Named
+data DeBruijn
+data XSKI
+
+type instance XLit Named a = ()
+type instance XFVar Named a = a
+type instance XBVar Named a = a
+type instance XAbs Named a = a
+type instance XApp Named a = ()
+type instance XExp Named a = ()
+type instance ScopeX b f Named a = f Named a
+
+
+type instance XLit  Named a = ()
+type instance XFVar Named a = a
+type instance XBVar Named a = a
+type instance XAbs Named a = a
+type instance XApp Named a = ()
+type instance XExp Named a = ()
+type instance ScopeX b f Named a = f Named a
+
+
+
+
+
